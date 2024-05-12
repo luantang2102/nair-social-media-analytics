87a57d96a8c7d97e6d4f9569b2d48e3948729428
LuanTang <luantang.work@gmail.com>
2024-05-12 22:04:52 +0700

    fixing the constants

---
+++ src/main/java/com/luantang/socialmediaanalytics/dashboard/constant/EdenAIConstant.java
@@ -0,0 +1,5 @@
+package com.luantang.socialmediaanalytics.dashboard.constant;
+
+public class EdenAIConstant {
+    public static final String API_KEY = "";
+}
---
+++ src/main/java/com/luantang/socialmediaanalytics/dashboard/constant/FacebookConstant.java
@@ -0,0 +1,9 @@
+package com.luantang.socialmediaanalytics.dashboard.constant;
+
+public class FacebookConstant {
+    public static final String FACEBOOK_GRAPH_API_URL = "https://graph.facebook.com/v19.0/";
+    public static final String FACEBOOK_OAUTH_ACCESS_TOKEN_URL = "https://graph.facebook.com/v19.0/oauth/access_token";
+    public static final String FACEBOOK_OAUTH_DIALOG_URL = "https://www.facebook.com/v19.0/dialog/oauth";
+    public static final String FACEBOOK_APP_ID = "";
+    public static final String FACEBOOK_APP_SECRET = "";
+}
